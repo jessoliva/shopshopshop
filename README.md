f66f7f6dc583b0b0dd8efe52b0b5d15eda3cb0e6
jessoliva <95152563+jessoliva@users.noreply.github.com>
2022-03-27 22:12:03 -0500

    added columns to category, product, producttag, tag

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # shopshopshop
+
+first change password and username in env file
+then mysql -u root -p and run source db/schema.sql
+then quit and in terminal run npm run seed
--- models/index.js
+++ models/index.js
@@ -4,13 +4,7 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
 
-// Categories have many Products
-
-// Products belongToMany Tags (through ProductTag)
-
-// Tags belongToMany Products (through ProductTag)
 
 module.exports = {
   Product,
